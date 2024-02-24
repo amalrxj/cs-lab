98158f1e2aa7a7ec5bbb517fa8c947c393f7a18c
Amal Raj <amalrajkvdev@gmail.com>
2024-02-24 19:04:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Bsc Computer Science Lab Programs
---
+++ largestThree.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, largest;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+    largest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+    printf("Largest number: %d",largest);
+
+    return 0;
+}
