ac360a3420a0030cdb397c051c80db8748b0a86f
amal <amalrajkvdev@gmail.com>
2024-06-07 09:26:09 +0530

    test commit

---
+++ assignment/pgrm5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int anuApples = 10, anuMangoes = 20;
+    int ammuApple = 5, ammuMangoes = 10;
+    int totalFruitFirst = anuApples + anuMangoes;
+    printf("Anu initially has %d apples and %d mangoes, totally %d fruits.\n", anuApples, anuMangoes, totalFruitFirst);
+    anuApples = ammuApple;
+    anuMangoes = ammuMangoes;
+    int totalFruitsRemaining = anuApples + anuMangoes;
+    printf("After sharing with ammu, anu has %d apples and %d mangoes left, totally %d fruits.\n", anuApples, anuMangoes, totalFruitsRemaining);
+
+    return 0;
+}
