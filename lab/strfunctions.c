0c766fb039dc1e4f9b7679db17a26e576b8a69cc
amal <amalrajkvdev@gmail.com>
2024-06-13 16:04:16 +0530

    gg

--- lab/strfunctions.c
+++ lab/strfunctions.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+
 int main()
 {
 	char str1[50],str2[50];
@@ -10,7 +11,7 @@ int main()
 	fgets(str2,sizeof(str2),stdin);
 	while(1)
 	{
-		printf("1 length\n2 copy\n3 concatenated\n4 compare");
+		printf("1 length\n2 copy\n3 concatenated\n4 reversed \n5 compare");
 		printf("\n enter your choice");
 		scanf("%d",&ch);
 		switch (ch)
@@ -23,7 +24,9 @@ int main()
 			        break;
 			case 3: printf("concatenated string '%s'\n",strcat(str1,str2));
 				    break;
-			case 4:result=strcmp(str1,str2);
+			case 4: printf("Reversed string: %s\n",strrev(str1));
+					break;	    
+			case 5: result=strcmp(str1,str2);
 			        if(result==0)
 			       {
 			       	 printf("'%s'and '%s'are equal\n ",str1,str2);
@@ -37,7 +40,7 @@ int main()
 						printf("'%s'is less than'%s'\n",str2,str1);
 					}	
 					break;	
-			default:printf("invalid");			    
+			default: printf("invalid");			    
 		}
 	}
 	return 0;
